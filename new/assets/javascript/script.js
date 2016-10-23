917cb8f436d5abbae09921992f1d675336585018
MrEdwinRodriguez <mredwinrodriguez@gmail.com>
2016-10-23 11:33:46 -0400

    add js to new

---
+++ new/assets/javascript/script.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function(){
+    $('.block').mouseover(function(){
+        console.log('hello world')
+         // $(this).toggle();
+        // $(this).animate({left: '100px'});
+        $(this).css('background', 'lightblue');
+        
+        });
+
+
+  
